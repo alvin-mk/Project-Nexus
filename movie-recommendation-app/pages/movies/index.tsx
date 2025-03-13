aebb2f7423a4803bb1d65f9c0c4062aa187128c1
Alvin K <ak.mbuthia98@gmail.com>
2025-03-13 18:43:56 +0400

    Footer component with copyright information

--- movie-recommendation-app/pages/movies/index.tsx
+++ movie-recommendation-app/pages/movies/index.tsx
@@ -122,7 +122,7 @@ const Movies: React.FC = () => {
 
       <div className="container mx-auto px-4 md:px-10 lg:px-44 py-16">
         <h1 className="text-3xl md:text-5xl font-bold mb-8">
-          {year || "All Years"} {genre} Movies
+          {year || "All Years"} {genre}  Movies
         </h1>
 
         {error && (
